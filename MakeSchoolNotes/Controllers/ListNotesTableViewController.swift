e37322c0efe776043128cb71981f9dc9b6f33150
Sneha P <snehafinches@gmail.com>
2018-07-09 11:55:14 -0700

    Initial commit

---
+++ MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ListNotesTableViewController.swift
+//  MakeSchoolNotes
+//
+//  Created by Chris Orcutt on 1/10/16.
+//  Copyright © 2016 MakeSchool. All rights reserved.
+//
+
+import UIKit
+class ListNotesTableViewController: UITableViewController {
+    var notes = [Note](){
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("this is a print")
+        
+        notes = CoreDataHelper.retrieveNotes()
+        //we want to retrieve and display all of the user's existing notes. We can do this by retrieve our existing notes and updating our notes array in ListNotesTableViewController at app launch.
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        /* Tells the UITableView how many UITableViewCell objects it should display. By default, each UITableView has 1 section */
+        // 1
+        return notes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        /* Returns the instance if the stylized UITableViewCell at a given IndexPath.
+         IndexPath is a data structure containing the cell's row and section indices */
+        // 2
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listNotesTableViewCell", for: indexPath) as! ListNotesTableViewCell
+        let note = notes[indexPath.row]
+        cell.noteTitleLabel.text = note.title
+        // 3
+        cell.noteModificationTimeLabel.text = note.modificationTime?.convertToString() ?? "unknown"
+
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let noteToDelete = notes[indexPath.row]
+            CoreDataHelper.delete(note: noteToDelete)
+            
+            notes = CoreDataHelper.retrieveNotes()
+            //we retrieve the note to be deleted corresponding to the selected index path. Then we use our Core Data helper to delete the selected note. Last we update our notes array to reflect the changes in our NSManagedObjectContext.
+        }
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let identifier = segue.identifier else { return }
+        
+        switch identifier {
+        case "displayNote":
+            // 1
+            guard let indexPath = tableView.indexPathForSelectedRow else { return }
+            
+            // 2
+            let note = notes[indexPath.row]
+            // 3
+            let destination = segue.destination as! DisplayNoteViewController
+            // 4
+            destination.note = note
+            
+        case "addNote":
+            print("create note bar button item tapped")
+            
+        default:
+            print("unexpected segue identifier")
+        }
+    }
+    @IBAction func unwindWithSegue(_ segue: UIStoryboardSegue) {
+        notes = CoreDataHelper.retrieveNotes()
+    }
+}
